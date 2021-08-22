e0753743ba46ed6fedf9d5091c1b5a0b5717e547
Sherry1481 <shareyscorpi1@gmail.com>
2021-08-23 03:42:56 +0500

    Initial Commit

---
+++ List/ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListApp.swift
+//  List
+//
+//  Created by sherry on 23/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
